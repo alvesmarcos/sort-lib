7dd48684032fb188cca4910d5ee6a5b3f2da3647
Marcos Alves <alves.henrique.marcos@gmail.com>
2016-08-08 02:11:33 -0300

    merge implemented and tested

--- src/sort.rb
+++ src/sort.rb
@@ -3,8 +3,9 @@
 # Author: Marcos Alves
 #++
 
+# MODULE SORT
+
 module Sort
-	#insertionsort
 	def self.insertion!(list)
 		len = list.length-1
 		
@@ -21,17 +22,48 @@ module Sort
 		end
 	end
 
-	#selectionsort
+	def self.fuse(left, right)
+		union = []
+		
+		until left.empty? || right.empty?
+			if left.first > right.first
+				union << right.shift
+			else
+				union << left.shift
+			end
+		end
+		union.concat(left).concat(right)
+	end
+	
+	def self.merge(list) 
+		return list if list.length <= 1
+
+		len = list.length/2
+		left = list.slice(0, len)
+		right = list.slice(len, list.length)
+
+		fuse(merge(left), merge(right))
+	end
+	
+	def self.merge!(list)
+		copy = []
+		copy << merge(list)
+		list.clear
+		(copy).each do |v|
+			list << v
+		end
+	end
+
 	def self.selection!(list)
 		len = list.length-1
 		
 		for i in 0..len-1
 			minimum = i	
 			for j in i+1..len
-				if list[minimum] > list[j] then minimum = j end
+				minimum = j if list[minimum] > list[j]
 			end			
 
-			if minimum != i then list[i], list[minimum] = list[minimum], list[i] end 
+			list[i], list[minimum] = list[minimum], list[i] if minimum != i
 		end
 	end
 end
--- src/tests.rb
+++ src/tests.rb
@@ -5,9 +5,12 @@
 
 require "./sort"
 
-numbers = Array.new(200) { rand(1..1000) } 
+numbers = Array.new(10) { rand(1..1000) } 
 
-Sort.insertion!(numbers)
+#Sort.insertion!(numbers)
+#Sort.heap!(numbers)
+Sort.merge!(numbers)
 #Sort.selection!(numbers)
+#Sort.quick!(numbers)
 
 puts numbers
